9ce612903bbbca80742df7ad8d5590a635c3e16f
arshan <arshankhanifar@gmail.com>
2023-02-17 12:47:00 -0500

    fuck around find out

--- src/Univ2Pool.sol
+++ src/Univ2Pool.sol
@@ -1,12 +1,40 @@
 import "forge-std/console2.sol";
+import "solmate/tokens/ERC20.sol";
 pragma solidity ^0.8.0;
 
+contract LPToken is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol, 18) {}
+}
+
 contract UniV2Pool {
+    ERC20 token0;
+    ERC20 token1;
+    ERC20 lpToken;
+    uint currPrice = 0;
 
-
-    constructor() {
-        console2.log("hey being built");
+    constructor(ERC20 _token0, ERC20 _token1) {
+        token0 = _token0;
+        token1 = _token1;
+        string memory name = string(
+            abi.encodePacked(token0.symbol(), "-", token1.symbol(), " LP Token")
+        );
+        string memory sym = string(
+            abi.encodePacked(token0.symbol(), "-", token1.symbol(), " LP")
+        );
+        lpToken = new LPToken(name, sym);
     }
 
-    function deposit() public {}
+
+
+    function deposit(uint256 amount0, uint256 amount1) public {
+        if (currPrice == 0) {
+            currPrice = amount1/amount0;
+        }
+
+        token0.transfer(msg.sender, this, amount1);
+        token1.transfer(msg.sender, this, amount2);
+    }
 }
--- test/UniV2Pool.t.sol
+++ test/UniV2Pool.t.sol
@@ -4,14 +4,19 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
 import "../src/Univ2Pool.sol";
+import "solmate/tokens/ERC20.sol";
+import "./mocks/MockErc20.sol";
 
 contract UniV2PoolTest is Test {
     UniV2Pool public pool;
+    ERC20 token0;
+    ERC20 token1;
 
     function setUp() public {
-        pool = new UniV2Pool();
+        token0 = new MockErc20("USD coin", "USDC");
+        token1 = new MockErc20("Tether", "USDT");
+        pool = new UniV2Pool(token0, token1);
     }
 
-    function testSetNumber(uint256 x) public {
-    }
+    function testSetNumber(uint256 x) public {}
 }
